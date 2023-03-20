1e284511e401b88223a199f0c5d179dc3c98b743
Nico Armanini <armanininico@gmail.com>
2023-03-20 17:34:46 -0300

    Agregar archivos de proyecto.

---
+++ frmConsulta.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace pryArmaniniAutoCor_SP1
+{
+    partial class frmConsulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mrcMarca1 = new System.Windows.Forms.Label();
+            this.cmbMarca1 = new System.Windows.Forms.ComboBox();
+            this.mrcOrigen = new System.Windows.Forms.GroupBox();
+            this.optNacional1 = new System.Windows.Forms.RadioButton();
+            this.optImportado1 = new System.Windows.Forms.RadioButton();
+            this.optAmbos = new System.Windows.Forms.RadioButton();
+            this.btnSalir1 = new System.Windows.Forms.Button();
+            this.btnConsultar1 = new System.Windows.Forms.Button();
+            this.dgvTabla = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mrcOrigen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mrcMarca1
+            // 
+            this.mrcMarca1.AutoSize = true;
+            this.mrcMarca1.Location = new System.Drawing.Point(35, 36);
+            this.mrcMarca1.Name = "mrcMarca1";
+            this.mrcMarca1.Size = new System.Drawing.Size(45, 16);
+            this.mrcMarca1.TabIndex = 0;
+            this.mrcMarca1.Text = "Marca";
+            // 
+            // cmbMarca1
+            // 
+            this.cmbMarca1.FormattingEnabled = true;
+            this.cmbMarca1.Items.AddRange(new object[] {
+            "Marca A",
+            "Marca B",
+            "Marca C"});
+            this.cmbMarca1.Location = new System.Drawing.Point(103, 33);
+            this.cmbMarca1.Name = "cmbMarca1";
+            this.cmbMarca1.Size = new System.Drawing.Size(240, 24);
+            this.cmbMarca1.TabIndex = 1;
+            // 
+            // mrcOrigen
+            // 
+            this.mrcOrigen.Controls.Add(this.optAmbos);
+            this.mrcOrigen.Controls.Add(this.optImportado1);
+            this.mrcOrigen.Controls.Add(this.optNacional1);
+            this.mrcOrigen.Location = new System.Drawing.Point(38, 82);
+            this.mrcOrigen.Name = "mrcOrigen";
+            this.mrcOrigen.Size = new System.Drawing.Size(356, 70);
+            this.mrcOrigen.TabIndex = 2;
+            this.mrcOrigen.TabStop = false;
+            this.mrcOrigen.Text = "Origen";
+            // 
+            // optNacional1
+            // 
+            this.optNacional1.AutoSize = true;
+            this.optNacional1.Location = new System.Drawing.Point(18, 33);
+            this.optNacional1.Name = "optNacional1";
+            this.optNacional1.Size = new System.Drawing.Size(82, 20);
+            this.optNacional1.TabIndex = 0;
+            this.optNacional1.TabStop = true;
+            this.optNacional1.Text = "Nacional";
+            this.optNacional1.UseVisualStyleBackColor = true;
+            // 
+            // optImportado1
+            // 
+            this.optImportado1.AutoSize = true;
+            this.optImportado1.Location = new System.Drawing.Point(132, 33);
+            this.optImportado1.Name = "optImportado1";
+            this.optImportado1.Size = new System.Drawing.Size(89, 20);
+            this.optImportado1.TabIndex = 1;
+            this.optImportado1.TabStop = true;
+            this.optImportado1.Text = "Importado";
+            this.optImportado1.UseVisualStyleBackColor = true;
+            // 
+            // optAmbos
+            // 
+            this.optAmbos.AutoSize = true;
+            this.optAmbos.Location = new System.Drawing.Point(252, 33);
+            this.optAmbos.Name = "optAmbos";
+            this.optAmbos.Size = new System.Drawing.Size(71, 20);
+            this.optAmbos.TabIndex = 2;
+            this.optAmbos.TabStop = true;
+            this.optAmbos.Text = "Ambos";
+            this.optAmbos.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir1
+            // 
+            this.btnSalir1.Location = new System.Drawing.Point(583, 82);
+            this.btnSalir1.Name = "btnSalir1";
+            this.btnSalir1.Size = new System.Drawing.Size(114, 37);
+            this.btnSalir1.TabIndex = 3;
+            this.btnSalir1.Text = "Salir";
+            this.btnSalir1.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar1
+            // 
+            this.btnConsultar1.Location = new System.Drawing.Point(583, 27);
+            this.btnConsultar1.Name = "btnConsultar1";
+            this.btnConsultar1.Size = new System.Drawing.Size(114, 37);
+            this.btnConsultar1.TabIndex = 4;
+            this.btnConsultar1.Text = "Consultar";
+            this.btnConsultar1.UseVisualStyleBackColor = true;
+            // 
+            // dgvTabla
+            // 
+            this.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvTabla.Location = new System.Drawing.Point(12, 183);
+            this.dgvTabla.Name = "dgvTabla";
+            this.dgvTabla.RowHeadersWidth = 51;
+            this.dgvTabla.RowTemplate.Height = 24;
+            this.dgvTabla.Size = new System.Drawing.Size(685, 314);
+            this.dgvTabla.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Codigo";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "Marca";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.Frozen = true;
+            this.Column4.HeaderText = "Origen";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.Frozen = true;
+            this.Column5.HeaderText = "Precio";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // frmConsulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(709, 509);
+            this.Controls.Add(this.dgvTabla);
+            this.Controls.Add(this.btnConsultar1);
+            this.Controls.Add(this.btnSalir1);
+            this.Controls.Add(this.mrcOrigen);
+            this.Controls.Add(this.cmbMarca1);
+            this.Controls.Add(this.mrcMarca1);
+            this.Name = "frmConsulta";
+            this.Text = "AUTOCOR - Consulta";
+            this.mrcOrigen.ResumeLayout(false);
+            this.mrcOrigen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mrcMarca1;
+        private System.Windows.Forms.ComboBox cmbMarca1;
+        private System.Windows.Forms.GroupBox mrcOrigen;
+        private System.Windows.Forms.RadioButton optAmbos;
+        private System.Windows.Forms.RadioButton optImportado1;
+        private System.Windows.Forms.RadioButton optNacional1;
+        private System.Windows.Forms.Button btnSalir1;
+        private System.Windows.Forms.Button btnConsultar1;
+        private System.Windows.Forms.DataGridView dgvTabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
---
+++ frmRepuestos.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace pryArmaniniAutoCor_SP1
+{
+    partial class frmRepuestos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mrcRepuesto = new System.Windows.Forms.GroupBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.optNacional = new System.Windows.Forms.RadioButton();
+            this.optImportado = new System.Windows.Forms.RadioButton();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.mrcRepuesto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcRepuesto
+            // 
+            this.mrcRepuesto.Controls.Add(this.optImportado);
+            this.mrcRepuesto.Controls.Add(this.optNacional);
+            this.mrcRepuesto.Controls.Add(this.txtPrecio);
+            this.mrcRepuesto.Controls.Add(this.cmbMarca);
+            this.mrcRepuesto.Controls.Add(this.txtNombre);
+            this.mrcRepuesto.Controls.Add(this.txtCodigo);
+            this.mrcRepuesto.Controls.Add(this.lblOrigen);
+            this.mrcRepuesto.Controls.Add(this.lblPrecio);
+            this.mrcRepuesto.Controls.Add(this.lblMarca);
+            this.mrcRepuesto.Controls.Add(this.lblNombre);
+            this.mrcRepuesto.Controls.Add(this.lblCodigo);
+            this.mrcRepuesto.Location = new System.Drawing.Point(33, 25);
+            this.mrcRepuesto.Name = "mrcRepuesto";
+            this.mrcRepuesto.Size = new System.Drawing.Size(387, 322);
+            this.mrcRepuesto.TabIndex = 0;
+            this.mrcRepuesto.TabStop = false;
+            this.mrcRepuesto.Text = "Repuesto";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(24, 53);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(51, 16);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(24, 111);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(24, 168);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(45, 16);
+            this.lblMarca.TabIndex = 2;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(24, 221);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(24, 272);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(47, 16);
+            this.lblOrigen.TabIndex = 4;
+            this.lblOrigen.Text = "Origen";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(127, 50);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(141, 22);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 105);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(208, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Items.AddRange(new object[] {
+            "Marca A",
+            "Marca B",
+            "Marca C"});
+            this.cmbMarca.Location = new System.Drawing.Point(127, 160);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(208, 24);
+            this.cmbMarca.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(127, 215);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(141, 22);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // optNacional
+            // 
+            this.optNacional.AutoSize = true;
+            this.optNacional.Location = new System.Drawing.Point(127, 270);
+            this.optNacional.Name = "optNacional";
+            this.optNacional.Size = new System.Drawing.Size(82, 20);
+            this.optNacional.TabIndex = 9;
+            this.optNacional.TabStop = true;
+            this.optNacional.Text = "Nacional";
+            this.optNacional.UseVisualStyleBackColor = true;
+            // 
+            // optImportado
+            // 
+            this.optImportado.AutoSize = true;
+            this.optImportado.Location = new System.Drawing.Point(231, 268);
+            this.optImportado.Name = "optImportado";
+            this.optImportado.Size = new System.Drawing.Size(89, 20);
+            this.optImportado.TabIndex = 10;
+            this.optImportado.TabStop = true;
+            this.optImportado.Text = "Importado";
+            this.optImportado.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(458, 43);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(117, 35);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(458, 118);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(117, 33);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(458, 199);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(117, 32);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(458, 283);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(117, 30);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmRepuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(591, 370);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.mrcRepuesto);
+            this.Name = "frmRepuestos";
+            this.Text = "AUTOCOR - Repuestos";
+            this.mrcRepuesto.ResumeLayout(false);
+            this.mrcRepuesto.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcRepuesto;
+        private System.Windows.Forms.RadioButton optImportado;
+        private System.Windows.Forms.RadioButton optNacional;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblOrigen;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
