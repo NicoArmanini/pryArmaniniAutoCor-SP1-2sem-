374e478f54ab93c7929c570b43ce7189a560c477
Alumnos <Alumnos@PC178>
2023-04-05 12:46:15 -0300

    zxd

--- frmConsulta.Designer.cs
+++ frmConsulta.Designer.cs
@@ -31,9 +31,9 @@
             this.mrcMarca1 = new System.Windows.Forms.Label();
             this.cmbMarca1 = new System.Windows.Forms.ComboBox();
             this.mrcOrigen = new System.Windows.Forms.GroupBox();
-            this.optNacional1 = new System.Windows.Forms.RadioButton();
-            this.optImportado1 = new System.Windows.Forms.RadioButton();
             this.optAmbos = new System.Windows.Forms.RadioButton();
+            this.optImportado1 = new System.Windows.Forms.RadioButton();
+            this.optNacional1 = new System.Windows.Forms.RadioButton();
             this.btnSalir1 = new System.Windows.Forms.Button();
             this.btnConsultar1 = new System.Windows.Forms.Button();
             this.dgvTabla = new System.Windows.Forms.DataGridView();
@@ -49,9 +49,10 @@
             // mrcMarca1
             // 
             this.mrcMarca1.AutoSize = true;
-            this.mrcMarca1.Location = new System.Drawing.Point(35, 36);
+            this.mrcMarca1.Location = new System.Drawing.Point(26, 29);
+            this.mrcMarca1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.mrcMarca1.Name = "mrcMarca1";
-            this.mrcMarca1.Size = new System.Drawing.Size(45, 16);
+            this.mrcMarca1.Size = new System.Drawing.Size(37, 13);
             this.mrcMarca1.TabIndex = 0;
             this.mrcMarca1.Text = "Marca";
             // 
@@ -62,9 +63,10 @@
             "Marca A",
             "Marca B",
             "Marca C"});
-            this.cmbMarca1.Location = new System.Drawing.Point(103, 33);
+            this.cmbMarca1.Location = new System.Drawing.Point(77, 27);
+            this.cmbMarca1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbMarca1.Name = "cmbMarca1";
-            this.cmbMarca1.Size = new System.Drawing.Size(240, 24);
+            this.cmbMarca1.Size = new System.Drawing.Size(181, 21);
             this.cmbMarca1.TabIndex = 1;
             // 
             // mrcOrigen
@@ -72,60 +74,67 @@
             this.mrcOrigen.Controls.Add(this.optAmbos);
             this.mrcOrigen.Controls.Add(this.optImportado1);
             this.mrcOrigen.Controls.Add(this.optNacional1);
-            this.mrcOrigen.Location = new System.Drawing.Point(38, 82);
+            this.mrcOrigen.Location = new System.Drawing.Point(28, 67);
+            this.mrcOrigen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mrcOrigen.Name = "mrcOrigen";
-            this.mrcOrigen.Size = new System.Drawing.Size(356, 70);
+            this.mrcOrigen.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mrcOrigen.Size = new System.Drawing.Size(267, 57);
             this.mrcOrigen.TabIndex = 2;
             this.mrcOrigen.TabStop = false;
             this.mrcOrigen.Text = "Origen";
             // 
-            // optNacional1
-            // 
-            this.optNacional1.AutoSize = true;
-            this.optNacional1.Location = new System.Drawing.Point(18, 33);
-            this.optNacional1.Name = "optNacional1";
-            this.optNacional1.Size = new System.Drawing.Size(82, 20);
-            this.optNacional1.TabIndex = 0;
-            this.optNacional1.TabStop = true;
-            this.optNacional1.Text = "Nacional";
-            this.optNacional1.UseVisualStyleBackColor = true;
-            // 
-            // optImportado1
-            // 
-            this.optImportado1.AutoSize = true;
-            this.optImportado1.Location = new System.Drawing.Point(132, 33);
-            this.optImportado1.Name = "optImportado1";
-            this.optImportado1.Size = new System.Drawing.Size(89, 20);
-            this.optImportado1.TabIndex = 1;
-            this.optImportado1.TabStop = true;
-            this.optImportado1.Text = "Importado";
-            this.optImportado1.UseVisualStyleBackColor = true;
-            // 
             // optAmbos
             // 
             this.optAmbos.AutoSize = true;
-            this.optAmbos.Location = new System.Drawing.Point(252, 33);
+            this.optAmbos.Location = new System.Drawing.Point(189, 27);
+            this.optAmbos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.optAmbos.Name = "optAmbos";
-            this.optAmbos.Size = new System.Drawing.Size(71, 20);
+            this.optAmbos.Size = new System.Drawing.Size(57, 17);
             this.optAmbos.TabIndex = 2;
             this.optAmbos.TabStop = true;
             this.optAmbos.Text = "Ambos";
             this.optAmbos.UseVisualStyleBackColor = true;
             // 
+            // optImportado1
+            // 
+            this.optImportado1.AutoSize = true;
+            this.optImportado1.Location = new System.Drawing.Point(99, 27);
+            this.optImportado1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.optImportado1.Name = "optImportado1";
+            this.optImportado1.Size = new System.Drawing.Size(72, 17);
+            this.optImportado1.TabIndex = 1;
+            this.optImportado1.TabStop = true;
+            this.optImportado1.Text = "Importado";
+            this.optImportado1.UseVisualStyleBackColor = true;
+            // 
+            // optNacional1
+            // 
+            this.optNacional1.AutoSize = true;
+            this.optNacional1.Location = new System.Drawing.Point(14, 27);
+            this.optNacional1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.optNacional1.Name = "optNacional1";
+            this.optNacional1.Size = new System.Drawing.Size(67, 17);
+            this.optNacional1.TabIndex = 0;
+            this.optNacional1.TabStop = true;
+            this.optNacional1.Text = "Nacional";
+            this.optNacional1.UseVisualStyleBackColor = true;
+            // 
             // btnSalir1
             // 
-            this.btnSalir1.Location = new System.Drawing.Point(583, 82);
+            this.btnSalir1.Location = new System.Drawing.Point(437, 67);
+            this.btnSalir1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir1.Name = "btnSalir1";
-            this.btnSalir1.Size = new System.Drawing.Size(114, 37);
+            this.btnSalir1.Size = new System.Drawing.Size(86, 30);
             this.btnSalir1.TabIndex = 3;
             this.btnSalir1.Text = "Salir";
             this.btnSalir1.UseVisualStyleBackColor = true;
             // 
             // btnConsultar1
             // 
-            this.btnConsultar1.Location = new System.Drawing.Point(583, 27);
+            this.btnConsultar1.Location = new System.Drawing.Point(437, 22);
+            this.btnConsultar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConsultar1.Name = "btnConsultar1";
-            this.btnConsultar1.Size = new System.Drawing.Size(114, 37);
+            this.btnConsultar1.Size = new System.Drawing.Size(86, 30);
             this.btnConsultar1.TabIndex = 4;
             this.btnConsultar1.Text = "Consultar";
             this.btnConsultar1.UseVisualStyleBackColor = true;
@@ -139,11 +148,12 @@
             this.Column3,
             this.Column4,
             this.Column5});
-            this.dgvTabla.Location = new System.Drawing.Point(12, 183);
+            this.dgvTabla.Location = new System.Drawing.Point(9, 149);
+            this.dgvTabla.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvTabla.Name = "dgvTabla";
             this.dgvTabla.RowHeadersWidth = 51;
             this.dgvTabla.RowTemplate.Height = 24;
-            this.dgvTabla.Size = new System.Drawing.Size(685, 314);
+            this.dgvTabla.Size = new System.Drawing.Size(649, 255);
             this.dgvTabla.TabIndex = 5;
             // 
             // Column1
@@ -193,16 +203,17 @@
             // 
             // frmConsulta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(709, 509);
+            this.ClientSize = new System.Drawing.Size(669, 414);
             this.Controls.Add(this.dgvTabla);
             this.Controls.Add(this.btnConsultar1);
             this.Controls.Add(this.btnSalir1);
             this.Controls.Add(this.mrcOrigen);
             this.Controls.Add(this.cmbMarca1);
             this.Controls.Add(this.mrcMarca1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmConsulta";
             this.Text = "AUTOCOR - Consulta";
             this.mrcOrigen.ResumeLayout(false);
